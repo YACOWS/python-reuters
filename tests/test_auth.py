3c95893d4e3f5abb34cff655cfdac6268ffb1c0a
Sergio Oliveira <sergio@tracy.com.br>
2012-11-26 17:49:52 -0200

    Testing authentication methods

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,7 +8,7 @@ class AuthenticationTest(ReutersTestCase):
     def test_not_authed(self):
         assert self.reuters._token is None
 
-    def test_auth(self):
+    def test_create_token(self):
         with open(self.get_datafile('headline_response.xml')) as response_file:
             self.reuters._request.return_value = response_file.read()
         assert self.reuters._create_service_token() == True
@@ -16,3 +16,22 @@ class AuthenticationTest(ReutersTestCase):
         with open(self.get_datafile('token')) as token_file:
             token = token_file.read().strip()
         assert self.reuters._token == token
+
+    def test_before_token_validation(self):
+        assert self.reuters.token_is_valid == False
+
+    def test_token_validation_false(self):
+        with open(self.get_datafile('token')) as token_file:
+            self.reuters._token = token_file.read().strip()
+
+        with open(self.get_datafile('validatetoken_false.xml')) as response:
+            self.reuters._request.return_value = response.read()
+        assert self.reuters.token_is_valid == False
+
+    def test_token_validation_true(self):
+        with open(self.get_datafile('token')) as token_file:
+            self.reuters._token = token_file.read().strip()
+
+        with open(self.get_datafile('validatetoken_true.xml')) as response:
+            self.reuters._request.return_value = response.read()
+        assert self.reuters.token_is_valid == True
